208d4f7ac8fc3915b357ad42662302e56241c65c
timothyAgevi <timothyagevi@gmail.com>
2021-07-23 13:02:38 +0300

    instructions

---
+++ password.py
@@ -0,0 +1,8 @@
+#Use Python script to generate a random password of 8 characters. Each time the program is run, a new password will be generated randomly. The passwords generated will be 8 characters long and will have to include the following characters in any order:
+
+# 2 uppercase letters from A to Z,
+# 2 lowercase letters from a to z,
+# 2 digits from 0 to 9,
+# 2 punctuation signs such as !, ?, “, # etc.
+
+# Hint: To solve this challenge we will have to generate random characters and to do so we will need to use the ASCII code.
