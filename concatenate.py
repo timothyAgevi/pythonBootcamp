0c199e100928f89662e1a923553d45f550554d51
timothyAgevi <timothyagevi@gmail.com>
2021-07-23 14:34:50 +0300

    concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,16 @@
+num =20
+num2 = 10
+sum = (num + num2) 
+print(f'number is '  +  '' + str(num) + ' ' + 'and sum ' + ' ' + str(sum))
+
+# contatenating integerg
+# method 1 : use of str()
+# method 2 : use of format
+# method 3 : special use of format
+
+# method 2 : use of format
+print(f' number is' +' ' + format(num,'.2f') + ' '+ 'and sum',sum)
+
+# method 3 : special use of format
+s ='{:.2f},{}'.format(num,sum)
+print(s)
--- password.py
+++ password.py
@@ -1,4 +1,8 @@
-#Use Python script to generate a random password of 8 characters. Each time the program is run, a new password will be generated randomly. The passwords generated will be 8 characters long and will have to include the following characters in any order:
+#Use Python script to generate a random password of 8 characters.
+#  Each time the program is run,
+#  a new password will be generated randomly. 
+# The passwords generated will be 8 characters long
+#  and will have to include the following characters in any order:
 
 # 2 uppercase letters from A to Z,
 # 2 lowercase letters from a to z,
