fbc1b21df478a821bc6157d3e8a4542ad40fbc16
timothyAgevi <timothyagevi@gmail.com>
2021-07-24 13:09:44 +0300

    uniform function

--- wk1/random.py
+++ wk1/random.py
@@ -4,8 +4,15 @@ import random
 print(random.random()) 
  # error : TypeError: 'module' object is not callable
 
-# to display 5 numbers  
+# to display 5 numbers  btn 0 and 1
 # for i in range(5):
 #     print(random.random())
 
-# kizungumkuti
+# to display 5 numbers  upto 6
+for i in range(5):
+    print(random.random()* 6)
+
+# using uniform 
+# 2 parameters ;num to statr from ;number to end
+for i in range(5):
+    print(random.uniform(1,6))
