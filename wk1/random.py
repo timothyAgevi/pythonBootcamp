e5c3ca101acbe57869185413caaec892f7fe2df5
timothyAgevi <timothyagevi@gmail.com>
2021-07-24 13:44:23 +0300

    add shuffle function

--- wk1/random.py
+++ wk1/random.py
@@ -35,3 +35,11 @@ print(random.choice(friends_list))
 # only draws sample once
 friends_list =  ['John', 'Eric', 'Michael', 'Terry', 'Graham']
 print(random.sample(friends_list,3))
+
+
+# 3 chaffle
+import random
+friends_list =  ['John', 'Eric', 'Michael', 'Terry', 'Graham']
+print(random.sample(friends_list,5))
+random.shuffle(friends_list)
+print(friends_list)
