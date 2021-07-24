089314b2bccf4f7baf84fbdcce4561e85031bfa6
timothyAgevi <timothyagevi@gmail.com>
2021-07-24 12:56:22 +0300

    random.py

--- wk1/random.py
+++ wk1/random.py
@@ -1,3 +1,11 @@
 # basic syntax
-import random
-for iprint(random.random())
+import random   
+
+print(random.random()) 
+ # error : TypeError: 'module' object is not callable
+
+# to display 5 numbers  
+# for i in range(5):
+#     print(random.random())
+
+# kizungumkuti
