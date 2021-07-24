1cee793f22f848e93fc4eef1671dd442ec31ef56
timothyAgevi <timothyagevi@gmail.com>
2021-07-24 15:30:20 +0300

    prime.py

---
+++ wk1/prime.py
@@ -0,0 +1,9 @@
+# prime numbers between 1 and 10
+#note in range(a,b) doesnt include b
+# prime mumber: positive number with only 2 divisors(1 and itself)
+# 1 is not a prime number
+print([ x for x in range(2,11)
+  if not any ([x % y == 0 for y in range(2,x)])])
+
+# print([x for x in range(2, 151) 
+# if not any([x % y == 0 for y in range(2, x)])])
