4ed030f4f07a70f5c4a5b9777975c1f9871937eb
timothyAgevi <timothyagevi@gmail.com>
2021-07-25 13:50:35 +0300

    action after condition

--- wk1/tradingGame.py
+++ wk1/tradingGame.py
@@ -1,4 +1,5 @@
 # draw a marble from a bag ( assume it random)
 # bag has 10 marbles : 6 green 4 red
 # if you draw a green marble you win same amount you bet,
-#if you draw red marble you  lose same amount you bet  
+#if you draw red marble you  lose same amount you bet 
+# marbles are replaced into bages after each round  
