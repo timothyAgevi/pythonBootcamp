3aaff1168ded7391ab359f6290d66a3acff2581b
timothyAgevi <timothyagevi@gmail.com>
2021-07-25 16:16:51 +0300

    commentout shorthand method

--- wk1/palindrome.py
+++ wk1/palindrome.py
@@ -11,5 +11,5 @@ def is_palindrome(val):
     else:
         return(False)
 
-def is_palindrome(val):
-    return str(val) == str(val)[::-1]
+# def is_palindrome(val):
+#     return str(val) == str(val)[::-1]
