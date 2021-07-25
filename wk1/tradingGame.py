3b66f1b24dc8041973f3611aecb16a4cb06e5526
timothyAgevi <timothyagevi@gmail.com>
2021-07-25 14:44:13 +0300

    actual pseudocode

--- wk1/tradingGame.py
+++ wk1/tradingGame.py
@@ -17,3 +17,18 @@
 #  1 red with a white 5X loser 
 
 import random
+#create a bag with 10 marbles
+#starting amount of money
+# current money amount
+#result of last round
+#how many rounds
+#last marble
+# welcome user to game
+# loop drawing marbles
+    #how much was bet
+    #draw marble
+    # win or loss
+    #calc win or loss/ result and new amount/purse
+    #lose game if lose half of money
+    #print results
+# print final results
