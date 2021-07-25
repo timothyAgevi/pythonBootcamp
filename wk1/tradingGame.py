eca94c3e22ab1b52ed1d4384a71b2986360162a9
timothyAgevi <timothyagevi@gmail.com>
2021-07-25 13:47:42 +0300

    add condition

--- wk1/tradingGame.py
+++ wk1/tradingGame.py
@@ -1,2 +1,4 @@
 # draw a marble from a bag ( assume it random)
 # bag has 10 marbles : 6 green 4 red
+# if you draw a green marble you win same amount you bet,
+#if you draw red marble you  lose same amount you bet  
