0d5afb2ffd5c1dfd251b3746660688f12ca2be29
timothyAgevi <timothyagevi@gmail.com>
2021-09-11 17:18:50 +0300

    login function

--- wk2/flaskLogin/app.py
+++ wk2/flaskLogin/app.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username in users and users[username][1] == password:
+            session["username"] = username
+            return redirect(url_for("home"))
+    return render_template("login.jinja2")
+
+
 @app.route('/')
 def index():
     return 'Hello World'
