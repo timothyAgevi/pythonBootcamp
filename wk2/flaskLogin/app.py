37f135da922caae88ff103213f1c299d42f27c8f
timothyAgevi <timothyagevi@gmail.com>
2021-09-11 15:27:40 +0300

    update

--- wk2/flaskLogin/app.py
+++ wk2/flaskLogin/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for, request,redirect;
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    
-    return "hello world" 
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__=='__main__':
+    app.run(debug = True)
+
+
 
 
 
